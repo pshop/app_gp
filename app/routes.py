a8308c6c24d4eae9b3a08c94fd5fdf41bafcbbfd
pshop <choppin.p@gmail.com>
2018-05-21 13:33:10 +0200

    Ajout du parser, Changement nom des modules

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
-from flask import (Flask, request, jsonify, render_template)
-                   
-app = Flask(__name__)
+from flask import render_template, jsonify, request
+from app import app
 
 @app.route('/')
 def index():
